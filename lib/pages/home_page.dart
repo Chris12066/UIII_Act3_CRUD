844684163c223924ca8460024496fe4aea1875c5
CHRISTIAN_ALEXIS_ALCANTARA_GONZALEZ <a.21308051280308@cbtis128.edu.mx>
2024-06-10 14:38:16 +0000

    actualizado

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:alcantara/pages/list_page.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<Widget> _pages = [
+    const PersonList(),
+  ];
+
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Firebase'),
+      ),
+      body: _pages[_selectedIndex],
+    );
+  }
+}
